4cb9ca7275d155ace3acc7991bc1b7786f9ecab2
davidkim310 <davidkim310@gmail.com>
2017-04-28 14:37:56 -0700

    adds route module for outsourcing routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '' , component: HomeComponent },
-  { path: 'users' , component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent},
-  ] },
-  
-  { path: 'servers' , component: ServersComponent, children:[
-    { path: ':id' , component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-  ] },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found' } //make sure this is always the last route listed
-];
+
 
 @NgModule({
   declarations: [
@@ -43,7 +32,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes) 
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
