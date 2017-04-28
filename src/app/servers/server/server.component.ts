a45603c93aba6332d3023dc0338261dfbe26648a
davidkim310 <davidkim310@gmail.com>
2017-04-28 14:21:09 -0700

    adds wildcard route and 404 page

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -12,7 +12,8 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -24,6 +25,10 @@ export class ServerComponent implements OnInit {
           //updates id on changes
         }
       );
+  }
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'}) //to which route do we want to route relatively
+    //makes sure old params are not lost
   }
 
 }
